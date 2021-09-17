51af1e0a6b4cee5e824a5c16a2cf850d1d2c432c
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2021-09-17 17:31:39 -0300

    Requisito 3 finalizado

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route path="/shopping-cart" component={ ShoppingCart } />
       </Switch>
     </BrowserRouter>
   );
