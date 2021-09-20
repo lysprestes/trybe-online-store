2960b30d5dd9a326fb686c6732a26262ddb69ae4
Écio Ferraz <ecioferraz@hotmail.com>
2021-09-20 16:50:49 -0300

    req 8 em andamento

--- src/components/ProductCards.jsx
+++ src/components/ProductCards.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import addToLocalStorage from '../services/addToLocalStorage';
+// import ShoppingCart from '../pages/ShoppingCart';
 
 export default class ProductCards extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleCart = this.handleCart.bind(this);
+  }
+
+  handleCart(title) {
+    addToLocalStorage(title);
+  }
+
   render() {
     const { products } = this.props;
     return (
@@ -11,6 +22,13 @@ export default class ProductCards extends React.Component {
             <img src={ thumbnail } alt={ `imagem ${title}` } />
             <h4>{ title }</h4>
             <p>{ price }</p>
+            <button
+              type="button"
+              data-testid="product-add-to-cart"
+              onClick={ () => this.handleCart(title) }
+            >
+              Adicionar ao carrinho
+            </button>
           </div>
         ))}
       </div>
--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
 export default class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cart: [],
+    };
+  }
+
   render() {
+    const { cart } = this.state;
+    if (!cart) {
+      return (
+        <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
+      );
+    }
     return (
-      <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
+      <>
+      </>
     );
   }
 }
